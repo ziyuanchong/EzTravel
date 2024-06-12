888a59196f65f49e7d8057f133a19699c77003a7
aeonsparkz <157774701+aeonsparkz@users.noreply.github.com>
2024-06-12 23:24:09 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/Register" element={<Login />} />
       </Routes>
     </>
   );
